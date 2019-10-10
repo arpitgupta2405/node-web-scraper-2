d2dce4018c26676321a6702772e28ba1c3487766
Arpit Gupta <arpit@blueberrylabs.com>
2019-10-10 13:16:10 +0530

    added the mysql db connectivity

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const webscraper = require('./lib/WebScrapper.js').WebScrapper;
 var WebScrapper = new webscraper();
 
 var websiteToScrape = "https://medium.com/";
-var fileName = "List_of_URLs.txt";
 
-WebScrapper.main(websiteToScrape, fileName);
+WebScrapper.main(websiteToScrape);
